7e763a070a0c882f561091bac497939307f948a0
54108 <3318195572@qq.com>
2024-11-06 21:54:51 +0800

    更新训练配置，添加优化器设置并注释掉步长配置,unext

--- net/model.py
+++ net/model.py
@@ -1,99 +1,377 @@
 import torch
-import torch.nn as nn
+from torch import nn
+import torch
+from torch import nn
 import torch.nn.functional as F
+import numpy as np
+import math
 
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
- 
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
- 
+__all__ = ['UNext']
+
+# import timm
+# from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+# import types
+
+# from abc import ABCMeta, abstractmethod
+# from mmcv.cnn import ConvModule
+# import pdb
+
+def trunc_normal_(tensor, mean=0.0, std=1.0, a=-2.0, b=2.0):
+    """
+    用截断正态分布初始化张量。
+    
+    参数:
+    tensor (torch.Tensor): 要初始化的张量。
+    mean (float): 正态分布的均值。
+    std (float): 正态分布的标准差。
+    a (float): 截断范围的下界。
+    b (float): 截断范围的上界。
+    """
+    with torch.no_grad():
+        # 计算截断范围的标准化值
+        lower = (a - mean) / std
+        upper = (b - mean) / std
+
+        # 使用截断正态分布生成随机数
+        tensor.uniform_(2 * lower - 1, 2 * upper - 1)
+        tensor = tensor.erfinv()
+        tensor = tensor * std * math.sqrt(2.0) + mean
+        tensor = torch.clamp(tensor, min=a, max=b)
+        return tensor
+
+class DropPath(nn.Module):
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
     def forward(self, x):
-        return self.double_conv(x)
-        
-        
-        
-class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
- 
-    def __init__(self, in_channels, out_channels):
+        if self.drop_prob == 0. or not self.training:
+            return x
+        keep_prob = 1 - self.drop_prob
+        shape = (x.shape[0],) + (1,) * (x.ndimension() - 1)
+        random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+        random_tensor.floor_()  # binarize
+        output = x.div(keep_prob) * random_tensor
+        return output
+
+def to_2tuple(x):
+    if isinstance(x, (tuple, list)):
+        assert len(x) == 2
+        return x
+    return (x, x)
+
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, bias=False)
+
+
+# def shift(dim):
+#             x_shift = [ torch.roll(x_c, shift, dim) for x_c, shift in zip(xs, range(-self.pad, self.pad+1))]
+#             x_cat = torch.cat(x_shift, 1)
+#             x_cat = torch.narrow(x_cat, 2, self.pad, H)
+#             x_cat = torch.narrow(x_cat, 3, self.pad, W)
+#             return x_cat
+
+class shiftmlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0., shift_size=5):
         super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
-        )
- 
-    def forward(self, x):
-        return self.maxpool_conv(x)
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.dim = in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.dwconv = DWConv(hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+        self.shift_size = shift_size
+        self.pad = shift_size // 2
+
+        
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+    
+    # def shift(x, dim):
+    #     x = F.pad(x, "constant", 0)
+    #     x = torch.chunk(x, self.shift_size, 1)
+    #     x = [ torch.roll(x_s, shift, dim) for x_s, shift in zip(x, range(-self.pad, self.pad+1))]
+    #     x = torch.cat(x, 1)
+    #     return x[:, :, self.pad:-self.pad, self.pad:-self.pad]
+
+    def forward(self, x, H, W):
+        # pdb.set_trace()
+        B, N, C = x.shape
+
+        xn = x.transpose(1, 2).view(B, C, H, W).contiguous()
+        xn = F.pad(xn, (self.pad, self.pad, self.pad, self.pad) , "constant", 0)
+        xs = torch.chunk(xn, self.shift_size, 1)
+        x_shift = [torch.roll(x_c, shift, 2) for x_c, shift in zip(xs, range(-self.pad, self.pad+1))]
+        x_cat = torch.cat(x_shift, 1)
+        x_cat = torch.narrow(x_cat, 2, self.pad, H)
+        x_s = torch.narrow(x_cat, 3, self.pad, W)
+
+
+        x_s = x_s.reshape(B,C,H*W).contiguous()
+        x_shift_r = x_s.transpose(1,2)
+
+
+        x = self.fc1(x_shift_r)
+
+        x = self.dwconv(x, H, W)
+        x = self.act(x) 
+        x = self.drop(x)
+
+        xn = x.transpose(1, 2).view(B, C, H, W).contiguous()
+        xn = F.pad(xn, (self.pad, self.pad, self.pad, self.pad) , "constant", 0)
+        xs = torch.chunk(xn, self.shift_size, 1)
+        x_shift = [torch.roll(x_c, shift, 3) for x_c, shift in zip(xs, range(-self.pad, self.pad+1))]
+        x_cat = torch.cat(x_shift, 1)
+        x_cat = torch.narrow(x_cat, 2, self.pad, H)
+        x_s = torch.narrow(x_cat, 3, self.pad, W)
+        x_s = x_s.reshape(B,C,H*W).contiguous()
+        x_shift_c = x_s.transpose(1,2)
+
+        x = self.fc2(x_shift_c)
+        x = self.drop(x)
+        return x
 
 
 
-
-class Up(nn.Module):
-    """Upscaling then double conv"""
- 
-    def __init__(self, in_channels, out_channels, bilinear=True):
+class shiftedBlock(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, sr_ratio=1):
         super().__init__()
- 
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
- 
-        self.conv = DoubleConv(in_channels, out_channels)
- 
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = torch.tensor([x2.size()[2] - x1.size()[2]])
-        diffX = torch.tensor([x2.size()[3] - x1.size()[3]])
- 
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
- 
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
-        
-        
-        
-class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
- 
+
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = shiftmlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def forward(self, x, H, W):
+
+        x = x + self.drop_path(self.mlp(self.norm2(x), H, W))
+        return x
+
+
+class DWConv(nn.Module):
+    def __init__(self, dim=768):
+        super(DWConv, self).__init__()
+        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        x = x.transpose(1, 2).view(B, C, H, W)
+        x = self.dwconv(x)
+        x = x.flatten(2).transpose(1, 2)
+
+        return x
+
+class OverlapPatchEmbed(nn.Module):
+    """ Image to Patch Embedding
+    """
+
+    def __init__(self, img_size=224, patch_size=7, stride=4, in_chans=3, embed_dim=768):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.H, self.W = img_size[0] // patch_size[0], img_size[1] // patch_size[1]
+        self.num_patches = self.H * self.W
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=stride,
+                              padding=(patch_size[0] // 2, patch_size[1] // 2))
+        self.norm = nn.LayerNorm(embed_dim)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
     def forward(self, x):
-        return self.conv(x)
-        
-        
-        
+        x = self.proj(x)
+        _, _, H, W = x.shape
+        x = x.flatten(2).transpose(1, 2)
+        x = self.norm(x)
+
+        return x, H, W
+
 class self_net(nn.Module):
-    def __init__(self, num_class = 4, channel = 3, bilinear=True):
-        super(self_net, self).__init__()
-        self.n_channels = channel
-        self.n_classes = num_class
-        self.bilinear = bilinear
- 
-        self.inc = DoubleConv(channel, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 128)
-        self.up1 = Up(256, 64, bilinear)
-        self.up2 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, num_class)
- 
+
+    ## Conv 3 + MLP 2 + shifted MLP w less parameters
+    
+    def __init__(self,  num_class=4, channel=3, deep_supervision=False,img_size=200, patch_size=16, in_chans=3,  embed_dims=[32, 64, 128, 512],
+                 num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=False, qk_scale=None, drop_rate=0.,
+                 attn_drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm,
+                 depths=[1, 1, 1], sr_ratios=[8, 4, 2, 1], **kwargs):
+        super().__init__()
+        
+        self.encoder1 = nn.Conv2d(3, 8, 3, stride=1, padding=1)  
+        self.encoder2 = nn.Conv2d(8, 16, 3, stride=1, padding=1)  
+        self.encoder3 = nn.Conv2d(16, 32, 3, stride=1, padding=1)
+
+        self.ebn1 = nn.BatchNorm2d(8)
+        self.ebn2 = nn.BatchNorm2d(16)
+        self.ebn3 = nn.BatchNorm2d(32)
+        
+        self.norm3 = norm_layer(embed_dims[1])
+        self.norm4 = norm_layer(embed_dims[2])
+
+        self.dnorm3 = norm_layer(64)
+        self.dnorm4 = norm_layer(32)
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
+
+        self.block1 = nn.ModuleList([shiftedBlock(
+            dim=embed_dims[1], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[0], norm_layer=norm_layer,
+            sr_ratio=sr_ratios[0])])
+
+        self.block2 = nn.ModuleList([shiftedBlock(
+            dim=embed_dims[2], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[1], norm_layer=norm_layer,
+            sr_ratio=sr_ratios[0])])
+
+        self.dblock1 = nn.ModuleList([shiftedBlock(
+            dim=embed_dims[1], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[0], norm_layer=norm_layer,
+            sr_ratio=sr_ratios[0])])
+
+        self.dblock2 = nn.ModuleList([shiftedBlock(
+            dim=embed_dims[0], num_heads=num_heads[0], mlp_ratio=1, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[1], norm_layer=norm_layer,
+            sr_ratio=sr_ratios[0])])
+
+        self.patch_embed3 = OverlapPatchEmbed(img_size=img_size // 4, patch_size=3, stride=2, in_chans=embed_dims[0],
+                                              embed_dim=embed_dims[1])
+        self.patch_embed4 = OverlapPatchEmbed(img_size=img_size // 8, patch_size=3, stride=2, in_chans=embed_dims[1],
+                                              embed_dim=embed_dims[2])
+
+        self.decoder1 = nn.Conv2d(128, 64, 3, stride=1,padding=1)  
+        self.decoder2 =   nn.Conv2d(64, 32, 3, stride=1, padding=1)  
+        self.decoder3 =   nn.Conv2d(32, 16, 3, stride=1, padding=1) 
+        self.decoder4 =   nn.Conv2d(16, 8, 3, stride=1, padding=1)
+        self.decoder5 =   nn.Conv2d(8, 8, 3, stride=1, padding=1)
+
+        self.dbn1 = nn.BatchNorm2d(64)
+        self.dbn2 = nn.BatchNorm2d(32)
+        self.dbn3 = nn.BatchNorm2d(16)
+        self.dbn4 = nn.BatchNorm2d(8)
+        
+        self.final = nn.Conv2d(8, num_class, kernel_size=1)
+
+        self.soft = nn.Softmax(dim =1)
+
     def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x = self.up1(x3, x2)
-        x = self.up2(x, x1)
-        logits = self.outc(x)
-        return logits
+        
+        B = x.shape[0]
+        ### Encoder
+        ### Conv Stage
+
+        ### Stage 1
+        out = F.relu(F.max_pool2d(self.ebn1(self.encoder1(x)),2,2))
+        t1 = out
+        ### Stage 2
+        out = F.relu(F.max_pool2d(self.ebn2(self.encoder2(out)),2,2))
+        t2 = out
+        ### Stage 3
+        out = F.relu(F.max_pool2d(self.ebn3(self.encoder3(out)),2,2))
+        t3 = out
+
+        ### Tokenized MLP Stage
+        ### Stage 4
+
+        out,H,W = self.patch_embed3(out)
+        for i, blk in enumerate(self.block1):
+            out = blk(out, H, W)
+        out = self.norm3(out)
+        out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        t4 = out
+
+        ### Bottleneck
+
+        out ,H,W= self.patch_embed4(out)
+        for i, blk in enumerate(self.block2):
+            out = blk(out, H, W)
+        out = self.norm4(out)
+        out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+
+        ### Stage 4
+
+        out = F.relu(F.interpolate(self.dbn1(self.decoder1(out)),size=t4.shape[2:], mode='bilinear', align_corners=False))
+        # out = F.interpolate(out, size=t4.shape[2:], mode='bilinear', align_corners=False)
+        out = torch.add(out,t4)
+        _,_,H,W = out.shape
+        out = out.flatten(2).transpose(1,2)
+        for i, blk in enumerate(self.dblock1):
+            out = blk(out, H, W)
+
+        ### Stage 3
+        
+        out = self.dnorm3(out)
+        out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        # out = F.interpolate(out, size=t3.shape[2:], mode='bilinear', align_corners=False)
+        out = F.relu(F.interpolate(self.dbn2(self.decoder2(out)), size=t3.shape[2:], mode='bilinear', align_corners=False))
+        out = torch.add(out,t3)
+        _,_,H,W = out.shape
+        out = out.flatten(2).transpose(1,2)
+        
+        for i, blk in enumerate(self.dblock2):
+            out = blk(out, H, W)
+
+        out = self.dnorm4(out)
+        out = out.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+
+        out = F.relu(F.interpolate(self.dbn3(self.decoder3(out)),scale_factor=(2,2),mode ='bilinear'))
+        out = torch.add(out,t2)
+        out = F.relu(F.interpolate(self.dbn4(self.decoder4(out)),scale_factor=(2,2),mode ='bilinear'))
+        out = torch.add(out,t1)
+        out = F.relu(F.interpolate(self.decoder5(out),scale_factor=(2,2),mode ='bilinear'))
+
+        return self.final(out)
+
+
+#EOF
